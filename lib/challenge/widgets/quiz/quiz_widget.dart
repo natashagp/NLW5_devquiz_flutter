baebd759e1ef2845588d56c323dd560f7d26e3ec
natashagp <natasha.paulon@gmail.com>
2021-04-20 17:37:08 -0300

    NLW5 Aula 02 - Maximum Speed

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,34 @@
+import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:devquiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(height: 24.0),
+          AnswerWidget(title: 'Kit de desenvolvimento de interface de usuário'),
+          AnswerWidget(
+              isRight: true,
+              isSelected: true,
+              title:
+                  'Possibilita a criação de aplicativos compilados nativamente'),
+          AnswerWidget(title: 'Acho que é uma marca de café do Himalaia'),
+          AnswerWidget(
+              title:
+                  'Possibilita a criação de desktops que são muito incríveis'),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:devquiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +15,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            SizedBox(height: 24.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: 'Fácil'),
+                LevelButtonWidget(label: 'Médio'),
+                LevelButtonWidget(label: 'Díficil'),
+                LevelButtonWidget(label: 'Perito'),
+              ],
+            ),
+            SizedBox(height: 24.0),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
